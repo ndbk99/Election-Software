652871828cd3634b7cc5cb6c655a05ddd7a7d804
NDBK <ndbk@columbus.rr.com>
2016-09-09 15:14:00 -0400

    Added App class to poll window program
    Enveloped entire window and elements in class App. Input is the window,
    and all creation of buttons/text/etc go in/are called in __init__.

--- poll_exe.py
+++ poll_exe.py
@@ -1,12 +1,29 @@
 from Tkinter import *
 
+class App:
+
+    def __init__(self,master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+        )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+        self.title = Label(master, text="Hello World!")
+        self.title.pack()
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
 # make window, app
 window = Tk()
 poll = App(window)
 
-# title
-title = Label(window, text="TITLE")
-title.pack()
-
 # end
 window.mainloop()
+window.destroy()
