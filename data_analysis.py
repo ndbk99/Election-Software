663f4f894479e4a5a88d697f065bcadd55216fed
NDBK <ndbk@columbus.rr.com>
2016-09-19 12:20:07 -0400

    Basic data retrieval function
    Wrote function retrieve_data to find voters who matched desired
    demographic and calculate how many answered a specific way to a specific
    question. Next, will need to refine how queries are looked up and make
    an easy user interface for entering them.

--- data_analysis.py
+++ data_analysis.py
@@ -17,6 +17,8 @@ class Poll:
         master: Tk GUI window to hold the analysis interface
         questions: list of questions asked in the poll
         voters: list of voters who responded to the poll
+        questions_file: CSV file holding poll questions (see parameter of same name)
+        voters_file: CSV file holding voters (see parameter of same name)
 
     Methods
     -------
@@ -25,14 +27,17 @@ class Poll:
         generate_window(): creates Tk GUI window with options for data analysis
     """
 
-    def __init__(self,master,questions_file,voters_file):
-        self.master = master
+    def __init__(self,questions_file,voters_file):
+        self.master = 0
         self.questions_file = questions_file
         self.voters_file = voters_file
         self.questions = self.read_questions()
         self.voters = self.read_voters()
 
     def read_questions(self):
+        """
+        Reads in info (topic, statement, answers) for each question asked in the poll.
+        """
         questions = []
         with open(self.questions_file) as csvfile:
             doc = csv.reader(csvfile)
@@ -41,6 +46,9 @@ class Poll:
         return questions
 
     def read_voters(self):
+        """
+        Reads in voter response data from CSV file.
+        """
         voters = []
         with open(self.voters_file) as csvfile:
             doc = csv.reader(csvfile)
@@ -49,11 +57,55 @@ class Poll:
                 voters.append(Voter(self,row))
         return voters
 
-    def generate_window(self):
-        pass
+    def initialize_poll(self):
+        """
+        Creates and runs a Poll instance.
+        Parameters: none
+        Returns: none
+        """
+        window = Tk()
+        self.master = window
+        self.read_questions()
+        self.read_voters()
+        print("%s voters" % len(csg_poll.voters))
 
-    def retrieve_data(self):
-        pass
+    def retrieve_data(self,demographics,question,answer):
+        """
+        Retrieves data on a question for a specific set of voters who fit 1 or more demographic constraints
+
+        Parameters
+        ----------
+        demographics: dictionary, with keys of demographic category name and values of demographic response
+        question: string, which is the topic for the desired question
+        answer: string, which is the answer that you want the percentage for
+
+        Returns
+        -------
+        percentage: percentage of voters in the demographic category who responded to the question with the given answer
+        """
+        print("RETRIEVING DATA")
+
+        # Retrieve voters whose demographics match desired
+        voters_counted = []
+        for voter in self.voters:
+            matches = False
+            for attr in demographics:
+                if voter.responses[attr] == demographics[attr]:
+                    matches = True
+                else:
+                    matches = False
+            if matches:
+                voters_counted.append(voter)
+
+        # Retrieve response % from voters in target demographic
+        total = len(voters_counted) * 1.0
+        matching_answer = 0.0
+        for voter in voters_counted:
+            if voter.responses[question] == answer:
+                matching_answer += 1.0
+        percentage = matching_answer / total * 100
+        print("%d of these voters (%d percent) answered %s with '%s'" % (matching_answer, percentage,question,answer))
+        return percentage
 
 
 class Voter:
@@ -66,17 +118,17 @@ class Voter:
 
     Attributes
     ----------
-        demographics: array of responses to questions about voter demographics
-        responses: array of responses to poll questions
+        Poll: poll object with which the voter is associated
+        responses: dictionary of responses to poll questions
 
     Methods
     -------
     """
 
-    def __init__(self,poll,responses):
+    def __init__(self,poll,row):
         self.Poll = poll
         questions = [q.topic for q in self.Poll.questions]
-        self.responses = dict(zip(questions, responses))
+        self.responses = dict(zip(questions, row[1:]))
 
 class Question:
     """
@@ -103,14 +155,9 @@ class Question:
         self.statement = row[1]
         self.responses = row[2:]
 
-def run_analysis():
-    window = Tk()
-    questions_file = "Poll_Questions.csv"
-    voters_file = "CSG PoliSci Sample Poll Data.csv"
-    csg_poll = Poll(window,questions_file,voters_file)
-    csg_poll.read_questions()
-    csg_poll.read_voters()
-    print("%s voters" % len(csg_poll.voters))
-    end = raw_input("END")
-
-run_analysis()
+questions_file = "Poll_Questions.csv"
+voters_file = "CSG PoliSci Sample Poll Data.csv"
+csg_poll = Poll(questions_file,voters_file)
+csg_poll.initialize_poll()
+csg_poll.retrieve_data({"School": "Academy"},"Party","Green")
+end = raw_input("END")
